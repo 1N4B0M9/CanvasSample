cc1546328db241b1ecdaf75ec995e4d79281214b
akshatsabavat <akshat.sabavat@gmail.com>
2025-09-16 23:12:36 -0400

    Defined: UI for new toolbar

--- src/components/Canvas/Components/Elements/ToolBar.jsx
+++ src/components/Canvas/Components/Elements/ToolBar.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useRef } from 'react';
+import { FaImages, FaShapes } from 'react-icons/fa6';
 import ImageSearch from './ImageSearch';
+import { IoImage, IoLayers } from 'react-icons/io5';
+import { PiSelectionBackgroundBold } from 'react-icons/pi';
+import { TbFileExport } from 'react-icons/tb';
+import { LuUserRound, LuUserRoundPlus } from 'react-icons/lu';
+import { BsFileEarmarkFont } from 'react-icons/bs';
+import { GrRedo, GrUndo } from 'react-icons/gr';
+import { RiDeleteBin5Line } from 'react-icons/ri';
 
 const ToolBar = ({
 	handleAddText,
@@ -197,8 +205,51 @@ const ToolBar = ({
 	};
 
 	return (
-		<div className="bg-red-500 absolute left-4 top-18 mt-4 z-50">
-			<p>Tool Bar here</p>
+		<div className="px-2 py-1 rounded-xl absolute left-4 top-18 mt-4 z-50 border border-1 border-gray-300 bg-white shadow flex flex-row">
+			{/* Base Tool Row*/}
+			<div className="flex flex-row gap-1 items-center">
+				{[
+					{ Icon: LuUserRoundPlus, label: 'Add Mentor' },
+					{ Icon: BsFileEarmarkFont, label: 'Add Attribute' },
+					{ Icon: IoImage, label: 'Add Image' },
+					{ Icon: IoLayers, label: 'Layers' },
+					{ Icon: PiSelectionBackgroundBold, label: 'Background' },
+					{ Icon: TbFileExport, label: 'Export' },
+				].map(({ Icon, label }, idx) => (
+					<div
+						key={label}
+						className="group rounded-md p-2 transition-colors duration-150 cursor-pointer hover:bg-blue-100"
+						title={label}
+					>
+						<Icon className="text-xl" />
+					</div>
+				))}
+			</div>
+			<div className="mx-2 border-l border-gray-300 h-8 self-center" />
+			<div className="flex flex-row items-center">
+				{/* Additional Tool Row */}
+				{[
+					{ Icon: GrUndo, label: 'Undo Action' },
+					{ Icon: GrRedo, label: 'Redo Action' },
+					{ Icon: RiDeleteBin5Line, label: 'Delete Element', isDelete: true },
+				].map(({ Icon, label, isDelete }, idx) => (
+					<div
+						key={label}
+						className={
+							isDelete
+								? 'group rounded-md p-2 transition-colors duration-150 cursor-pointer bg-white hover:bg-red-100'
+								: 'group rounded-md p-2 transition-colors duration-150 cursor-pointer hover:bg-blue-100'
+						}
+						title={label}
+					>
+						<Icon
+							className={
+								isDelete ? 'text-xl text-red-300 group-hover:text-red-600 transition-colors duration-150' : 'text-xl'
+							}
+						/>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
